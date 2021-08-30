f67073234995fa126844d39b3e3adc58b68b9f20
Ky Nguyen <nguyentruongky33@gmail.com>
2021-08-30 09:46:44 +0700

    add: change routers

--- Numerade/Routers.swift
+++ Numerade/Routers.swift
@@ -8,36 +8,47 @@ enum Destination {
     case problemDetail(book: Book?, chapter: Chapter?, problem: Problem)
 }
 
+enum PresentType {
+    case present, push
+}
+
 extension UIViewController {
     func push(to destination: Destination) {
+        show(presentType: .push, destination: destination)
+    }
+
+    func present(destination: Destination) {
+        show(presentType: .present, destination: destination)
+    }
+
+    private func show(presentType: PresentType = .push, destination: Destination) {
         switch destination {
             case .bookDetail(let book):
                 let vc = BookDetailController()
                 vc.book = book
-                navigationController?.pushViewController(vc, animated: true)
-            
+                show(presentType: presentType, screen: vc)
+
             case .chapterDetail(let book, let chapter):
                 let vc = ChapterDetailController()
                 vc.book = book
                 vc.chapter = chapter
-                navigationController?.pushViewController(vc, animated: true)
+                show(presentType: presentType, screen: vc)
 
-            default:
-                break
-        }
-    }
-    
-    func present(destination: Destination) {
-        switch destination {
             case .problemDetail(let book, let chapter, let problem):
                 let vc = ProblemDetailController()
                 vc.book = book
                 vc.chapter = chapter
                 vc.problem = problem
-                present(vc, animated: true)
+                show(presentType: presentType, screen: vc)
+        }
+    }
 
-            default:
-                break
+    private func show(presentType: PresentType, screen: UIViewController) {
+        switch presentType {
+            case .present:
+                present(screen, animated: true)
+            case .push:
+                navigationController?.pushViewController(screen, animated: true)
         }
     }
 }
