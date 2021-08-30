45f1b3a7e0dec1d6e62e50fb874eb567863aebcd
Ky Nguyen <nguyentruongky33@gmail.com>
2021-08-30 07:17:01 +0700

    add: Routers to navigate around screens

--- Numerade/Routers.swift
+++ Numerade/Routers.swift
@@ -1,8 +1,43 @@
-//
-//  Routers.swift
-//  Numerade
-//
-//  Created by Ky Nguyen on 8/30/21.
-//
+//  Created by Ky Nguyen
 
-import Foundation
+import UIKit
+
+enum Destination {
+    case bookDetail(book: Book)
+    case chapterDetail(book: Book?, chapter: Chapter)
+    case problemDetail(book: Book?, chapter: Chapter?, problem: Problem)
+}
+
+extension UIViewController {
+    func push(to destination: Destination) {
+        switch destination {
+            case .bookDetail(let book):
+                let vc = BookDetailController()
+                vc.book = book
+                navigationController?.pushViewController(vc, animated: true)
+            
+            case .chapterDetail(let book, let chapter):
+                let vc = ChapterDetailController()
+                vc.book = book
+                vc.chapter = chapter
+                navigationController?.pushViewController(vc, animated: true)
+
+            default:
+                break
+        }
+    }
+    
+    func present(destination: Destination) {
+        switch destination {
+            case .problemDetail(let book, let chapter, let problem):
+                let vc = ProblemDetailController()
+                vc.book = book
+                vc.chapter = chapter
+                vc.problem = problem
+                present(vc, animated: true)
+
+            default:
+                break
+        }
+    }
+}
